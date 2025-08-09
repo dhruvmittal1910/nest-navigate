78b53817240c6c641b96bb81d86b7bf26cd99d6a
Dhruv Mittal <dhruvmittal1910@gmail.com>
2025-08-09 01:19:01 -0400

    remove profile.tsx import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from './components/Login';
 import { Dashboard } from './components/Dashboard';
-import { Profile } from './components/Profile';
+// import { Profile } from './components/Profile';
 import {CreateModule} from "./components/CreateModule"
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path='/profile' element={<Profile />} />
+        {/* <Route path='/profile' element={<Profile />} /> */}
         <Route path='/module' element={<CreateModule/>}/>
       </Routes>
     </Router>
